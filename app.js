903ad392ab089594a4043d8eee0827122f81f88e
Spring-Mirage <spring-mirage>
2024-11-16 18:37:39 -0500

    Feat: Mouse Over and Mouse Out

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ new Vue({
         name: 'Bitcoin',
         img: 'https://img.icons8.com/?size=100&id=8OVvETCMTPDi&format=png&color=000000',
         changePercent: -10,
-        prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
+        // prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
 
         pricesWithDays: [
             { day: 'Lunes', value: 8400 },
@@ -15,6 +15,13 @@ new Vue({
             { day: 'Sábado', value: 10000 },
             { day: 'Domingo', value: 10200 },
         ],
+
+        showPrices: false,
     },
 
+    methods: {
+        toggleShowPrices() {
+            this.showPrices = !this.showPrices
+        }
+    },
 })
