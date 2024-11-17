f904295fc2ae2415abf8b6c17e3bd6a4b5aa56b9
Spring-Mirage <spring-mirage>
2024-11-17 16:51:01 -0500

    Feat: Children and slot

--- Module1/app.js
+++ Module1/app.js
@@ -1,48 +1,11 @@
-new Vue({
-    el: '#app',
-    data: {
-        name: 'Bitcoin',
-        symbol: 'BTC',
-        img: 'https://img.icons8.com/?size=100&id=8OVvETCMTPDi&format=png&color=000000',
-        changePercent: -10,
-        // prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
+Vue.component('CoinDetails', {
 
-        value: 0,
+    props: ['coin'],
 
-        color: 'f4f4f4',
-
-        price: 8400,
-
-        pricesWithDays: [
-            { day: 'Lunes', value: 8400 },
-            { day: 'Martes', value: 7900 },
-            { day: 'Miércoles', value: 8200 },
-            { day: 'Jueves', value: 9000 },
-            { day: 'Viernes', value: 9400 },
-            { day: 'Sábado', value: 10000 },
-            { day: 'Domingo', value: 10200 },
-        ],
-
-        showPrices: false,
-    },
-
-    computed: {
-        title() {
-            return `${this.name} - ${this.symbol}`
-        },
-
-        convertedValue() {
-            if (!this.value) {
-                return 0
-            }
-
-            return this.value / this.price
-        }
-    },
-
-    watch: {
-        showPrices(newVal, oldVal) {
-            console.log(newVal, oldVal)
+    data() {
+        return {
+            showPrices: false,
+            value: 0
         }
     },
 
@@ -50,9 +13,112 @@ new Vue({
         toggleShowPrices() {
             this.showPrices = !this.showPrices
 
-            this.color = this.color.split('')
+            this.$emit('change-color', this.showPrices ? 'FFF6CC' : '3D3D3D')
+        }
+    },
+
+    computed: {
+        title() {
+            return `${this.coin.name} - ${this.coin.symbol}`
+        },
+
+        convertedValue() {
+            if (!this.value) {
+                return 0
+            }
+
+            return this.value / this.coin.price
+        }
+    },
+
+    template: `
+        <div>
+            <img 
+                v-on:mouseover="toggleShowPrices" 
+                v-on:mouseout="toggleShowPrices" 
+                v-bind:src="coin.img" 
+                v-bind:alt="coin.name"
+            >
+            <h1 v-bind:class="coin.changePercent > 0 ? 'green' : 'red' ">
+                
+                {{ title }}
+                <span v-if="coin.changePercent > 0">👍</span>
+                <span v-else-if="coin.changePercent < 0">👎</span>
+                <span v-else>🤞</span>
+
+                <span v-on:click="toggleShowPrices">{{ showPrices ? '🙈' : '🐵' }}</span>
+                
+            </h1>
+
+            <input type="number" v-model="value">
+            <span>{{ convertedValue }}</span>
+
+            <slot name="text"></slot>
+            <slot name="link"></slot>
+
+            <ul v-show="showPrices">
+                <li 
+                    class="uppercase"
+                    v-bind:class="{ orange: p.value === coin.price, red: p.value < coin.price, green: p.value > coin.price }"
+                    v-for="(p, i) in coin.pricesWithDays" 
+                    v-bind:key="p.day"
+                >
+                    {{ i }} - {{ p.day }} {{ p.value }}
+                </li>
+            </ul>
+        </div>
+    `
+})
+
+new Vue({
+
+    el: '#app',
+
+    data: {
+        btc: {
+            name: 'Bitcoin',
+            symbol: 'BTC',
+            img: 'https://img.icons8.com/?size=100&id=8OVvETCMTPDi&format=png&color=000000',
+            changePercent: -10,
+            // prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
+            price: 8400,
+
+            pricesWithDays: [
+                { day: 'Lunes', value: 8400 },
+                { day: 'Martes', value: 7900 },
+                { day: 'Miércoles', value: 8200 },
+                { day: 'Jueves', value: 9000 },
+                { day: 'Viernes', value: 9400 },
+                { day: 'Sábado', value: 10000 },
+                { day: 'Domingo', value: 10200 },
+            ],
+        },
+
+        color: 'f4f4f4',
+
+    },
+
+    methods: {
+        updateColor(color) {
+            this.color = color || this.color
             .reverse()
             .join('')
         }
     },
+
+    // watch: {
+    //     showPrices(newVal, oldVal) {
+    //         console.log(newVal, oldVal)
+    //     }
+    // },
+
+    // methods: {
+    //     toggleShowPrices() {
+    //         this.showPrices = !this.showPrices
+
+    //         this.color = this.color.split('')
+    //         .reverse()
+    //         .join('')
+    //     }
+    // },
 })
