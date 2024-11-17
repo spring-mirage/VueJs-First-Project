c895085199d5f10e86ac14a9f5c191937ebc183b
Spring-Mirage <spring-mirage>
2024-11-16 20:13:16 -0500

    Feat: Computed and watch

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ new Vue({
     el: '#app',
     data: {
         name: 'Bitcoin',
+        symbol: 'BTC',
         img: 'https://img.icons8.com/?size=100&id=8OVvETCMTPDi&format=png&color=000000',
         changePercent: -10,
         // prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
@@ -21,6 +22,18 @@ new Vue({
         ],
 
         showPrices: false,
+    },
+
+    computed: {
+        title() {
+            return `${this.name} - ${this.symbol}`
+        },
+    },
+
+    watch: {
+        showPrices(newVal, oldVal) {
+            console.log(newVal, oldVal)
+        }
     },
 
     methods: {
