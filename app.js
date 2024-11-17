bbc5f9326b49274c8e8a2006d6e1475b79c692d0
Spring-Mirage <spring-mirage>
2024-11-16 19:39:52 -0500

    Feat: V-BIND with color

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ new Vue({
         changePercent: -10,
         // prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
 
+        color: 'f4f4f4',
+
         price: 8400,
 
         pricesWithDays: [
@@ -24,6 +26,10 @@ new Vue({
     methods: {
         toggleShowPrices() {
             this.showPrices = !this.showPrices
+
+            this.color = this.color.split('')
+            .reverse()
+            .join('')
         }
     },
 })
